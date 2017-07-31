baeb88358df4f42d8824f76137f4eb2a72848db5
root <root@build.test.lo>
2017-07-31 10:23:45 +0000

    docker login before build added

--- run_state.sh
+++ run_state.sh
@@ -7,7 +7,7 @@ fi
 
 case $1 in 
   docker-install ) salt-call --local --file-root=/srv/salt-masterless/antibio/roots/ state.apply docker-install;;
-  build ) salt-call --local --file-root=/srv/salt-masterless/antibio/roots/ state.apply build;;
+  build ) docker login && salt-call --local --file-root=/srv/salt-masterless/antibio/roots/ state.apply build;;
   run ) salt-call --local --file-root=/srv/salt-masterless/antibio/roots/ state.apply run;;
   * ) echo "usage: $0 docker-install | build | run";exit 1
 esac
